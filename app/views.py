9377f87317958c2b7f05c193b9d4b51123817158
Ralf Michiels <ralf.michiels@axsguard.com>
2022-07-01 13:50:38 +0200

    Add function to read NVIDIA json file

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
+from dataclasses import dataclass
+import os
+from re import L
+import subprocess
 from app import app
-from flask import render_template
+from flask import render_template, request, json, jsonify, redirect
 
 
 @app.route("/")
@@ -10,3 +14,18 @@ def index():
 @app.route("/about")
 def about():
     return render_template("public/about.html")
+
+# url -X POST http://localhost:5000/json -H "Content-Type: application/json" -d @data.json
+
+
+@app.route("/getjson", methods=["GET", "POST"])
+def handle_json():
+    if (request.method == 'POST') and request.is_json:
+        req = request.get_json()
+        return "JSON received", 200
+    elif (request.method == 'GET'):
+        nvidia = subprocess.run(['cat', '/tmp/nvidia.json'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        return (nvidia)
+        # return render_template("public/getjson.html", nvidia=str)
+    else:
+        return "Invalid request!", 400
